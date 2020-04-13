15f7f16347157514b2d993fd416f15bf9812c6fc
Rodrigo-Fontoura <45045275+Rodrigo-Fontoura@users.noreply.github.com>
2020-04-13 19:58:01 -0300

    funcao para criar db

---
+++ functions/create_df_userid.R
@@ -0,0 +1,54 @@
+
+
+source('app/functions_app/set_key.R')
+source('functions/csgo_api.R')
+source("packages.r")
+
+
+create_df_stats_user <- function(api_key,user_id){
+  
+  suporte_armas <- read_excel("data/armas csgo.xlsx",1) %>% mutate(DESC= toupper(DESC))
+  suporte_mapas <- read_excel("data/armas csgo.xlsx",2) %>% mutate(DESC= toupper(DESC))
+  suporte_stats <- read_excel("data/armas csgo.xlsx",3) %>% mutate(DESC= toupper(DESC))
+  
+  stats <- csgo_api_stats(api_key,user_id)
+  
+  stats2 <- stats
+  
+  stats2$label = NA
+  stats2$type = NA
+  
+  #labels para armas
+  for(i in 1:nrow(suporte_armas)){
+    
+    pos <- grepl(suporte_armas$ARMA[i],stats2$name)
+    
+    stats2$label[pos] <- suporte_armas$DESC[i]
+    stats2$type[pos] <- "weapon info"
+  }
+  
+  
+  #labels para mapas
+  for(i in 1:nrow(suporte_mapas)){
+    
+    pos <- grepl(suporte_mapas$MAPA[i],stats2$name)
+    
+    stats2$label[pos] <- suporte_mapas$DESC[i]
+    stats2$type[pos] <- suporte_mapas$CATEGORIA[i]
+  }
+  
+  #labels para stats geral
+  for(i in 1:nrow(suporte_stats)){
+    
+    pos <- which(suporte_stats$STAT[i]==stats2$name)
+    
+    stats2$label[pos] <- suporte_stats$DESC[i]
+    stats2$type[pos] <- suporte_stats$CATEGORIA[i]
+  }
+  
+  stats2 <- stats2 %>% filter(!is.na(label))
+  
+    lista_db <- list(db_raw=stats,db_clean=stats2)
+  
+}
+
--- script/flags_variaveis.R
+++ script/flags_variaveis.R
@@ -6,26 +6,40 @@ source("packages.r")
 
 user_id <- '76561198263364899'
 
+suporte_armas <- read_excel("data/armas csgo.xlsx",1) %>% mutate(DESC= toupper(DESC))
+suporte_mapas <- read_excel("data/armas csgo.xlsx",2) %>% mutate(DESC= toupper(DESC))
+suporte_stats <- read_excel("data/armas csgo.xlsx",3) %>% mutate(DESC= toupper(DESC))
+
 
 stats <- csgo_api_stats(api_key,user_id)
 
-stats <- stats %>% mutate(category = case_when(grepl("total_wins_map",name) ~ "rounds vencidos mapa",
-                          grepl("total_rounds_map",name) ~ "rounds mapa",
-                          grepl("total_kills",name) ~ "kills com arma X",
-                          grepl("total_planted_bombs",name) ~ "C4 plantadas",
-                          grepl("total_defused_bombs",name) ~ "C4 desarmadas",
-                          grepl("total_wins_pistolround",name) ~ "vitorias em pistol round",
-                          grepl("total_weapons_donated",name) ~ "armas doadas",
-                          grepl("total_weapons_donated",name) ~ "armas doadas",
-                          grepl("total_shots_",name) ~ "tiros disparados",
-                          grepl("total_hits_",name) ~ "tiros certos",
-                          name== "total_shots_hit" ~ "tiros certos total",
-                          name=="total_matches_won" ~ "total de vitorias",
-                          name=="total_mvps" ~ "total MVPS",
-                          grepl("total_matches_won_",name) ~ "total de vitorias no mapa",
-                          name== "total_matches_played","total de partidas jogadas",
-                          name=="total_deaths" ~ "total de mortes",
-                          name=="total_damage_done" ~ "total de dano causado",
-                          name=="	total_progressive_matches_won" ~ "total de vitorias consecutivas"
-                           ))
+stats$label = NA
+stats$type = NA
 
+#labels para armas
+  for(i in 1:nrow(suporte_armas)){
+    
+    pos <- grepl(suporte_armas$ARMA[i],stats$name)
+    
+    stats$label[pos] <- suporte_armas$DESC[i]
+    stats$type[pos] <- "weapon info"
+  }
+
+
+#labels para mapas
+  for(i in 1:nrow(suporte_mapas)){
+    
+    pos <- grepl(suporte_mapas$MAPA[i],stats$name)
+    
+    stats$label[pos] <- suporte_mapas$DESC[i]
+    stats$type[pos] <- suporte_mapas$CATEGORIA[i]
+  }
+
+#labels para stats geral
+  for(i in 1:nrow(suporte_stats)){
+    
+    pos <- which(suporte_stats$STAT[i]==stats$name)
+    
+    stats$label[pos] <- suporte_stats$DESC[i]
+    stats$type[pos] <- suporte_stats$CATEGORIA[i]
+  }
