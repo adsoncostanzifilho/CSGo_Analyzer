5b1a8521314dcbb732232a756a36b3d650104006
Rodrigo-Fontoura <45045275+Rodrigo-Fontoura@users.noreply.github.com>
2020-04-14 22:53:15 -0300

    editar funcoes

--- functions/create_df_friends.R
+++ functions/create_df_friends.R
@@ -5,15 +5,18 @@ source('functions/csgo_api.R')
 source('functions/create_df_userid.R')
 source("packages.r")
 
-user_id <- '76561198263364899'
+create_df_stats_user <- function(api_key,user_name){
 
+user_id <- as.character(csgo_api_profile_by_name(api_key,user_name)) ##user_name vira do shiny app quando o usuario digitar
 
 friend_list <- csgo_api_friend(api_key,user_id)
 
 friends_permission_list <- list()
 
 
-  #for(i)
+  for(i in 1:nrow(friend_list)){
+    
+  }
 
 friend_data_list <- list()
 
@@ -24,3 +27,6 @@ friend_data_list <- list()
     friend_data_list[i] <- create_df_stats_user(api_key,user)
     
   }
+
+
+}
