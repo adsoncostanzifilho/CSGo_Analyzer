d99b516c8833910cf67e1565fc4d120613b02a69
Rodrigo-Fontoura <45045275+Rodrigo-Fontoura@users.noreply.github.com>
2020-04-21 12:59:50 -0300

    df friends
    criando df para amigos

--- functions/create_df_friends.R
+++ functions/create_df_friends.R
@@ -5,26 +5,53 @@ source('functions/csgo_api.R')
 source('functions/create_df_userid.R')
 source("packages.r")
 
-create_df_stats_user <- function(api_key,user_name){
+create_df_stats_friends <- function(api_key,user_name){
 
 user_id <- as.character(csgo_api_profile_by_name(api_key,user_name)) ##user_name vira do shiny app quando o usuario digitar
 
 friend_list <- csgo_api_friend(api_key,user_id)
 
-friends_permission_list <- list()
+#friends_permission_list <- list()
 
 
   for(i in 1:nrow(friend_list)){
     
+    temp <- csgo_api_profile(api_key,friend_list$steamid[i])
+    
+    if(("commentpermission" %in% colnames(temp))){
+        
+        friend_list$public[i] <- ifelse(temp$commentpermission == 1,"Public","Not Public")
+    }
+    else{
+      
+      friend_list$public[i] <- "Not Public"
+      
+    }
   }
+    
+
+
+friend_list <- friend_list %>% filter(public == "Public")
+
 
 friend_data_list <- list()
 
-  for(i in 1:1){
+
+  if(nrow(friend_list) >0){
+
+    for(i in 1:nrow(friend_list)){
+      
+      user = as.character(friend_list$steamid[i])
+      
+      friend_data_list[[i]] <- create_df_stats_user(api_key,user)
+      
+    }
     
-    user = as.character(friend_list$steamid[i])
+    return(list(friend_data_list,"Database was created!"))
+  }
+  else{
     
-    friend_data_list[i] <- create_df_stats_user(api_key,user)
+    return(list(friend_data_list,"There is no public data for friends of this steam user"))
     
   }
 
